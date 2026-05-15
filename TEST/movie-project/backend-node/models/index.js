d649370515ee032ca009bbf4a4a4947f8c914cf1
PhatDat <ngotranphatdat@gmail.com>
2026-05-15 14:27:59 +0700

    add movie into import and export

--- TEST/movie-project/backend-node/models/index.js
+++ TEST/movie-project/backend-node/models/index.js
@@ -1,8 +1,9 @@
 import user from "./user.js";
-import content from "./content.js";
+// import content from "./content.js";
 import comment from "./comments.js";
 import genre from "./genres.js";
 import rating from "./ratings.js";
+import movie from "./movie.js";
 import watchHistory from "./watchHistory.js";
 
 export default {
@@ -10,5 +11,6 @@ export default {
   Comment: comment,
   Genre: genre,
   Rating: rating,
+  Movie: movie,
   WatchHistory: watchHistory,
 };
